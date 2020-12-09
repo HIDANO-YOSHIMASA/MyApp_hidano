8483cad32e0258fefebe69db23f45b4705be6745
HIDANO-YOSHIMASA <hidano.yoshimasa@gmail.com>
2020-12-09 13:54:11 +0900

    カルーセルfirst add

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,29 +1,72 @@
 import React from "react";
-import { StyleSheet, Text, View, Button, SafeAreaView, ImageBackground, Platform } from "react-native";
+import { StyleSheet, Text, View, Button, SafeAreaView, ImageBackground, Platform, Linking, TouchableHighlight, Image, Dimensions } from "react-native";
 import {StackNavigationProp} from '@react-navigation/stack';
 import logo from "../assets/sparta.jpg";
 import Pic01 from "../assets/hamburger.jpg";
+import Carousel, { Pagination } from "react-native-snap-carousel";
 
 type Props = {
   navigation: StackNavigationProp<RootStackParamList, 'Home'>;
 }
 
 export default function HomeScreen({navigation}: Props) {
+
+const state = {
+  data: [
+    { title: "a", uri: "http://www.bluecode.jp/images/A.png" },
+    { title: "b", uri: "http://www.bluecode.jp/images/B.png" },
+    { title: "c", uri: "http://www.bluecode.jp/images/C.png" },
+    { title: "d", uri: "http://www.bluecode.jp/images/D.png" },
+    { title: "e", uri: "http://www.bluecode.jp/images/E.png" },
+  ],
+  activeSlide: 0,
+};
+
+const renderItem = ({ item, index }) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.topContainer}>
-        <Text style={{fontSize: 12, letterSpacing: 2, color: 'white', marginBottom: 20,}}>Contemporary Restaurant</Text>
-      </View>
+    <TouchableHighlight onPress={() => Linking.openURL(item.url)}>
+      <Image
+        source={{ uri: item.uri }}
+        style={{ width: "100%", height: "100%" }}
+      />
+    </TouchableHighlight>
+  );
+};
 
-        <ImageBackground
-          source={Pic01}
-          style={styles.backgroundImage} />
-        <Button style={styles.buttons} title="to Menu" onPress={() => navigation.navigate("Menu")} />
 
-      <View style={styles.bottomContainer}>
-        <Button title="ログイン" onPress={() => navigation.navigate("Menu")} />
-        <Button title="新規登録" onPress={() => navigation.navigate("Menu")} />
-      </View>
+  return (
+    // <SafeAreaView style={styles.container}>
+    //   <View style={styles.topContainer}>
+    //     <Text style={{fontSize: 12, letterSpacing: 2, color: 'white', marginBottom: 20,}}>Contemporary Restaurant</Text>
+    //   </View>
+
+    //     <ImageBackground
+    //       source={Pic01}
+    //       style={styles.backgroundImage} />
+    //     <Button style={styles.buttons} title="to Menu" onPress={() => navigation.navigate("Menu")} />
+
+    //   <View style={styles.bottomContainer}>
+    //     <Button title="ログイン" onPress={() => navigation.navigate("Menu")} />
+    //     <Button title="新規登録" onPress={() => navigation.navigate("Menu")} />
+    //   </View>
+    // </SafeAreaView>
+
+    <SafeAreaView style={{ height: 240 }}>
+      <Carousel
+        data={state.data}
+        renderItem={renderItem}
+        itemWidth={Dimensions.get("window").width * 0.6}
+        sliderWidth={Dimensions.get("window").width * 1.0}
+        // containerCustomStyle={{ flex: 1, backgroundColor: "#eee" }}
+        onSnapToItem={(index) => setState({ activeSlide: index })} //for pagination
+        loop
+        autoplay
+      />
+      <Pagination
+        dotsLength={state.data.length} //dotの数
+        activeDotIndex={state.activeSlide} //どのdotをactiveにするか
+        containerStyle={{ paddingVertical: 15 }} //デフォルトではちと広い
+      />
     </SafeAreaView>
   );
 }
